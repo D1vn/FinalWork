e770e28364a5e1e4155544b845cc0d2af4f915f4
Ivan <shal1b@yandex.ru>
2026-04-26 12:24:25 +0300

    Changes in Lab9.cpp

--- Lab9.cpp
+++ Lab9.cpp
@@ -7,48 +7,48 @@
 int main() {
 	system("chcp 1251");
 
-	struct Ball_znach {
-		char doZachet[16];
-		char posleZachet[16];
+	struct Ball_znach1 {
+		char doZachet1[16];
+		char posleZachet1[16];
 	};
 	struct Table{
-		int nomer;
-		char name[64];
-		char familya[64];
-		struct Ball_znach Ball;
+		int nomer1;
+		char name1[64];
+		char familya1[64];
+		struct Ball_znach1 Ball1;
 	};
 
-	int num;
+	int num1;
 	printf("Введите кол-во студентов до 7:");
-	scanf("%d", &num);
-	struct Table tablica[8], zamen;
-	for (int i = 0; i < num; i++) {
+	scanf("%d", &num1);
+	struct Table1 tablica1[8], zamen1;
+	for (int i = 0; i < num1; i++) {
 		printf("Введите номер студента:");
-		scanf("%d", &tablica[i].nomer);
+		scanf("%d", &tablica1[i].nomer1);
 		printf("Имя:");
-		scanf("%s", &tablica[i].name);
+		scanf("%s", &tablica1[i].name1);
 		printf("Фамилия:");
-		scanf("%s", &tablica[i].familya);
+		scanf("%s", &tablica1[i].familya1);
 		printf("Балл до зачета:");
-		scanf("%s", &tablica[i].Ball.doZachet);
+		scanf("%s", &tablica1[i].Ball.doZachet1);
 		printf("Балл после зачета:");
-		scanf("%s", &tablica[i].Ball.posleZachet);
+		scanf("%s", &tablica1[i].Ball.posleZachet1);
 	}
 	printf("Исходный массив:\n");
 	printf(" №  Фамилия         Имя               До / После \n");
-	for (int i = 0; i < num; i++) {
-		printf("%2d  %-14s  %-14s  %6s / %-8s\n", tablica[i].nomer, tablica[i].name, tablica[i].familya, tablica[i].Ball.doZachet, tablica[i].Ball.posleZachet);
+	for (int i = 0; i < num1; i++) {
+		printf("%2d  %-14s  %-14s  %6s / %-8s\n", tablica1[i].nomer1, tablica1[i].name1, tablica1[i].familya1, tablica1[i].Ball1.doZachet1, tablica1[i].Ball1.posleZachet1);
 		
 	}
-	if (num > 1) {
-		zamen = tablica[0];
-		tablica[0] = tablica[num - 1];
-		tablica[num - 1] = zamen;
+	if (num1 > 1) {
+		zamen1 = tablica1[0];
+		tablica1[0] = tablica1[num1 - 1];
+		tablica1[num1 - 1] = zamen1;
 	}
 	printf("Массив после перестановки:\n");
 	printf(" №  Фамилия         Имя               До / После \n");
-	for (int i = 0; i < num; i++) {
-		printf("%2d  %-14s  %-14s  %6s / %-8s\n", tablica[i].nomer, tablica[i].name, tablica[i].familya, tablica[i].Ball.doZachet, tablica[i].Ball.posleZachet);
+	for (int i = 0; i < num1; i++) {
+		printf("%2d  %-14s  %-14s  %6s / %-8s\n", tablica1[i].nomer1, tablica1[i].name1, tablica1[i].familya1, tablica1[i].Ball1.doZachet1, tablica1[i].Ball1.posleZachet1);
 
 	}
 }
